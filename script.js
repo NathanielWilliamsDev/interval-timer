f901c7f2ef6be67691b6695ce6d7e0f1c69e105e
Nathaniel <nathaniel.c.williams@hotmail.com>
2024-05-26 20:37:57 +1000

    Implemented basic javascript logic to take input from the input box, check the input is valid, assign the valid input (time) as a number of seconds in the h1 with class .countdown

--- script.js
+++ script.js
@@ -1,30 +1,50 @@
 const input = document.querySelector(".timer-controls input");
 const button = document.querySelector(".timer-controls button");
+const countdown = document.querySelector(".countdown");
 let counter = 0;
 button.addEventListener("click", ()=>{
     counter = input.value;
-    console.log(counter);
+    console.log(isCorrectTime(counter));
     input.value = "";
 });
 
 input.addEventListener("keydown", (e)=>{
     if(e.key === "Enter"){
         counter = input.value;
-        console.log(counter);
+        if(isCorrectTime(counter)){
+            countdown.textContent = convertToSeconds(counter);
+        }
+        else{
+            console.log("Invalid input given. Please use mm:ss, and only input Numbers");
+        }
         input.value = "";
     }
 });
 
 
 // Checks that the time is valid
+// time will be input.value
+// returns true if the time parameter is valid, false otherwise
 function isCorrectTime(time){
-
+    if(time.length != 5){ // user is forced to use mm:ss formatting
+        return(false);
+    }
+    else{
+        let minutesSeconds = time.split(":"); // splitting minutes and seconds
+        if(isNaN(Number(minutesSeconds[0])) || isNaN(Number(minutesSeconds[1]))){ // if either are NaN, then they weren't a number
+            return(false);
+        }
+        else if(minutesSeconds[0] < 0 || minutesSeconds[1] < 0){
+            return(false);
+        }
+        return(true); // both minute's and seconds must be a number, return them
+    }
 }
 
 // Concert time parameter into seconds
-// split on ":"
-// if produced list from split is > 1 (then you have minutes to convert)
-// otherwise if < 1, return the seconds
 function convertToSeconds(time){
-
+    let minutesSeconds = time.split(":"); // split minutes from seconds
+    let seconds = Number(minutesSeconds[1]); // store the number of seconds in a variable
+    seconds += (Number(minutesSeconds[0])*60); // convert minutes to seconds, add it to the seconds variable
+    return seconds;
 }
